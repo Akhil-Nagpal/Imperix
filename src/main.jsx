7448d7c6933edaa7b5c9b702b56da6b9acf4509c
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-05-13 21:16:26 +0530

    Added React Query & Dev Tools

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Root from './router/Root.jsx'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Search from './pages/Search.jsx'
-import Categories from './pages/Categories.jsx'
-import Contact from './pages/Contact.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import Root from "./layout/Root.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Search from "./pages/Search.jsx";
+import Categories from "./pages/Categories.jsx";
+import Contact from "./pages/Contact.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,12 +28,13 @@ const router = createBrowserRouter(
       <Route path="/contact" element={<Contact />} />
     </Route>
   )
-)
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}>
-     <App />
-    </RouterProvider>
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);

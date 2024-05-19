84ac2a6d4d05aa824f3a4a29cddcf4adb1139735
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-05-20 01:48:53 +0530

    Added intersection observer & Logic of infinite queries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 import unsplashApi from "../utils/api";
 import MasonaryGrid from "../components/MasonaryGrid";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 function Home() {
-  const { data } = useQuery({
-    queryKey: ["randomPhotos"],
-    queryFn: () => unsplashApi.get(`/photos`),
-    retry: 2,
-    staleTime: Infinity,
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
+  const fetchData = async ({ pageParam }) => {
+    console.log(pageParam);
+    const { data } = await unsplashApi({
+      url: "/photos",
+      params: {
+        page: pageParam,
+      },
+    });
+    console.log(data);
+    return data;
+  };
+
+  const {
+    data,
+    status,
+    error,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["images"],
+    queryFn: fetchData,
+    initialPageParam: 1,
+    getNextPageParam: (_, allPages) => allPages.length + 1,
   });
+
+  const allPhotos = data?.pages?.flat();
 
   return (
     <>
-      <MasonaryGrid param={data?.data} />
+      <MasonaryGrid param={allPhotos} />
+
+      <button
+        className="bg-gray-200 text-lg py-2 px-4 rounded-lg"
+        onClick={() => fetchNextPage()}
+      >
+        Load More
+      </button>
     </>
   );
 }

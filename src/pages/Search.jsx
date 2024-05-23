2900a50fd793469d9030eb3d79d0f673562462bc
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-05-23 22:19:41 +0530

    Refactored Code

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router-dom";
 function Search() {
   return (
     <>
-      <div>Search</div>
       <Outlet />
     </>
   );
--- src/pages/SearchPhotos.jsx
+++ src/pages/SearchPhotos.jsx
@@ -1,28 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import MasonaryGrid from "../components/MasonaryGrid";
-import pexelApi from "../utils/api";
+import { pexelApi } from "../utils/api";
+import { useInView } from "react-intersection-observer";
 
 function SearchPhotos() {
   let { query } = useParams();
 
-  const { data: search } = useQuery({
-    queryKey: ["photos", query],
-    queryFn: () => pexelApi.get(`/search?query=${query}`),
-    enabled: !!query,
-    retry: false,
-    staleTime: Infinity,
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
+  const [images, setImages] = useState([]);
+
+  const uniqueIds = new Set();
+
+  const fetchData = async ({ pageParam }) => {
+    const { data } = await pexelApi({
+      url: "/search",
+      params: {
+        query: query,
+        page: pageParam,
+      },
+    });
+
+    return data?.photos;
+  };
+
+  const { ref, inView } = useInView();
+
+  const {
+    data,
+    status,
+    error,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["images", query],
+    queryFn: fetchData,
+    retry: 2,
+    initialPageParam: 1,
+    getNextPageParam: (_, allPages) => allPages.length + 1,
   });
 
-  console.log(search);
+  useEffect(() => {
+    let uniqueArray = data?.pages.flat(Infinity).filter((obj) => {
+      if (!uniqueIds.has(obj.id)) {
+        uniqueIds.add(obj.id);
+        return true;
+      }
+      return false;
+    });
+
+    setImages(uniqueArray);
+  }, [data]);
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
   return (
     <>
       <h1 className="text-4xl font-bold capitalize">{query}</h1>
-      <MasonaryGrid param={search?.data} />
+      <div className="columns-2 md:columns-3 2xl:columns-4">
+        {images?.map((item) => (
+          <MasonaryGrid
+            key={item?.id}
+            imgUrl={item?.src?.large}
+            imgAlt={item?.alt}
+          />
+        ))}
+      </div>
+
+      {isFetchingNextPage && (
+        <div className="flex justify-center items-center h-80">Loading...</div>
+      )}
+      <div className="w-10 h-10" ref={ref}></div>
     </>
   );
 }
@@ -33,3 +86,15 @@ export default SearchPhotos;
 // 1. Photos Masonary Grid
 // 2. Tags
 // 3. Stats Navbar
+
+// const { data: search } = useQuery({
+//   queryKey: ["photos", query],
+//   queryFn: () => pexelApi.get(`/search?query=${query}`),
+//   enabled: !!query,
+//   retry: false,
+//   staleTime: Infinity,
+//   refetchOnMount: false,
+//   refetchOnWindowFocus: false,
+// });
+
+// console.log(search);
